d5f863544596cdb3cc01984459f34ec37f64cfad
Branislav Hesko <branislav.hesko@protonmail.ch>
2021-04-16 22:46:49 +0200

    fixed training

--- segmentation_trainer.py
+++ segmentation_trainer.py
@@ -29,12 +29,15 @@ class SegmentationTrainer:
         self._data_loader = get_data_loaders(self._cfg)
         self._loss = torch.nn.CrossEntropyLoss()
         self._writer = SummaryWriter()
+        self.load()
 
     def train(self):
         for epoch in range(self._cfg.num_epochs):
             self._train_single_epoch(epoch)
             if epoch % self._cfg.validation_frequency == 0:
                 self.validate(epoch)
+            self._scheduler.step()
+        self.save()
 
     def _train_single_epoch(self, epoch):
         self._model.train()
@@ -49,9 +52,10 @@ class SegmentationTrainer:
             loss.backward()
             self._optimizer.step()
             dice_value, iou_value = self._get_metrics(masks, model_output)
-            progress_bar.set_description("TRAINING - Epoch: {}, inference time: {:.2f} ms, iou: {:.2f}, "
-                                         "loss: {:.2f}".format(epoch, (time() - start) * 1000, iou_value, loss.item()))
-            self._write_scalar_to_tensorboard(dice_value, epoch, index, loss)
+            progress_bar.set_description("TRAINING - Epoch: {}, inference time: {:.2f} ms, iou: {:.2f}, dice: {:.2f} "
+                                         "loss: {:.2f}".format(epoch, (time() - start) * 1000,
+                                                               iou_value, dice_value, loss.item()))
+            self._write_scalar_to_tensorboard(dice_value, iou_value, epoch, index, loss)
 
     @torch.no_grad()
     def validate(self, epoch):
@@ -64,9 +68,10 @@ class SegmentationTrainer:
             model_output = self._model(images)
             loss = self._loss(model_output, masks)
             dice_value, iou_value = self._get_metrics(masks, model_output)
-            progress_bar.set_description("VALIDATION - Epoch: {}, inference time: {:.2f} ms, iou: {:.2f}, "
-                                         "loss: {:.2f}".format(epoch, (time() - start) * 1000, iou_value, loss.item()))
-            self._write_scalar_to_tensorboard(dice_value, epoch, index, loss)
+            progress_bar.set_description("VALIDATION - Epoch: {}, inference time: {:.2f} ms, iou: {:.2f}, dice: {:.2f} "
+                                         "loss: {:.2f}".format(epoch, (time() - start) * 1000,
+                                                               iou_value, dice_value, loss.item()))
+            self._write_scalar_to_tensorboard(dice_value, iou_value, epoch, index, loss)
 
     @staticmethod
     def _get_metrics(masks, model_output):
@@ -74,14 +79,16 @@ class SegmentationTrainer:
         dice_value = dice(masks, torch.argmax(model_output, dim=1))
         return dice_value, iou_value
 
-    def _write_scalar_to_tensorboard(self, dice_value, epoch, index, loss, mode=DataMode.train):
+    def _write_scalar_to_tensorboard(self, dice_value, iou_value, epoch, index, loss, mode=DataMode.train):
         mode = "Training" if mode == DataMode.train else "Validation"
         self._writer.add_scalar(f"{mode}Loss", loss.item(), epoch * len(self._data_loader[DataMode.train]) + index)
-        self._writer.add_scalar(f"{mode}IOU", iou, epoch * len(self._data_loader[DataMode.train]) + index)
+        self._writer.add_scalar(f"{mode}IOU", iou_value, epoch * len(self._data_loader[DataMode.train]) + index)
         self._writer.add_scalar(f"{mode}Dice", dice_value, epoch * len(self._data_loader[DataMode.train]) + index)
 
     def save(self):
         path = os.path.join(self._cfg.checkpoint_path, self._cfg.EXPERIMENT_NAME, "checkpoint.pth")
+        if not os.path.exists(os.path.split(path)[0]):
+            os.makedirs(os.path.split(path)[0], exist_ok=True)
         model_info = {
             "model": self._model.state_dict(),
             "optimizer": self._optimizer.state_dict(),
@@ -92,6 +99,8 @@ class SegmentationTrainer:
 
     def load(self):
         path = os.path.join(self._cfg.checkpoint_path, self._cfg.EXPERIMENT_NAME, "checkpoint.pth")
+        if not os.path.isfile(path):
+            return
         model_info = torch.load(path)
         self._model.load_state_dict(model_info["model"])
         if "optimizer" in model_info:
